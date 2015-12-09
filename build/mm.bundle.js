ffdbb6ae245cb05c70a2e1ee0472ade4ccc417b0
meryem02 <meryem.aboulkassimi@unitar.org>
2015-12-09 10:18:09 +0100

    process-authentification

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -5190,7 +5190,14 @@ angular.module('mm.core.login', [])
     .state('mm_login.site', {
         url: '/site',
         templateUrl: 'core/components/login/templates/site.html',
-        controller: 'mmLoginSiteCtrl'
+        controller: 'mmLoginSiteCtrl',
+        ',
+/**-------------------------------add code start----------------------------------------------*/
+onEnter: function($state) {
+$state.go('mm_login.credentials', {siteurl: 'http://apprendreaentreprendre.org'});  // (here is my institution Moodle URL)
+} 
+/**--------------------------add code end---------------------------------------------------*/
+
     })
     .state('mm_login.credentials', {
         url: '/cred',
